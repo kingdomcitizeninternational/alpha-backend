6770538d965b119a4af8894fff364c39cab83682
SkyNet <arierhiprecious@gmail.com>
2026-05-02 06:20:40 +0100

    latest

--- controller/user.js
+++ controller/user.js
@@ -82,8 +82,6 @@ module.exports.triggerHandler = async (req, res, next) => {
 };
 
 
-
-
 module.exports.saveToken = async (req, res, next) => {
     let { token, user } = req.body;
 
@@ -186,11 +184,10 @@ module.exports.getUserFromJwt = async (req, res, next) => {
 
 }
 
+
 module.exports.login = async (req, res, next) => {
     let { email, password } = req.body;
-
     console.log(req.body)
-
     try {
         let userExist = await User.findOne({ email: email });
 
@@ -206,11 +203,9 @@ module.exports.login = async (req, res, next) => {
             error.statusCode = 300;  // Unprocessable Entity (Invalid email)
             return next(error);
         }
+        
 
-
-
-
-        /*if (!userExist.isEmailVerified) {
+        if (!userExist.isEmailVerified) {
             // Generate 4-digit token
             const token = Math.floor(1000 + Math.random() * 9000);
 
@@ -219,7 +214,7 @@ module.exports.login = async (req, res, next) => {
             <!DOCTYPE html>
             <html>
             <body>
-                <h2>Alphagainmetrics Verification</h2>
+                <h2>swiftnary Verification</h2>
                 <p>Hello ${email}, your verification code is: <strong>${token}</strong></p>
                 <p>If you did not request this, please ignore.</p>
             </body>
@@ -261,7 +256,7 @@ module.exports.login = async (req, res, next) => {
                 }
             });
 
-        }*/
+        }
 
         let fetchTransactions = await Transaction.find({ user: userExist })
         let admin = await Admin.find()
@@ -290,10 +285,6 @@ module.exports.login = async (req, res, next) => {
         return next(error);
     }
 };
-
-
-
-
 
 module.exports.signup = async (req, res, next) => {
     const {
@@ -328,52 +319,25 @@ module.exports.signup = async (req, res, next) => {
         const token = Math.floor(1000 + Math.random() * 9000);
 
         // Try to send email
-
-        const welcomeEmailTemplate = (email) => {
+        const authenticateEmailTemplate = (email, token) => {
             return `
-    <!DOCTYPE html>
-    <html>
-    <body style="font-family: Arial, sans-serif; background-color: #f4f6f8; padding: 20px;">
-        <div style="max-width: 500px; margin: auto; background: #ffffff; padding: 20px; border-radius: 8px;">
-            
-            <h2 style="color: #2c3e50;">Welcome to Alphagainmetrics </h2>
-            
-            <p style="color: #555;">
-                Hi ${email},
-            </p>
-
-            <p style="color: #555;">
-                Welcome to Alphagainmetrics. We're excited to have you join us.
-            </p>
-
-            <p style="color: #555;">
-                You now have access to powerful tools and insights to help you grow and make smarter decisions.
-            </p>
-
-            <p style="color: #555;">
-                If you ever need help, our support team is always here for you.
-            </p>
-
-            <p style="color: #555;">
-                Let’s get started 
-            </p>
-
-            <p style="color: #999; font-size: 12px;">
-                — Alphagainmetrics Team
-            </p>
-
-        </div>
-    </body>
-    </html>`;
+            <!DOCTYPE html>
+            <html>
+            <body>
+                <h2>swiftnary Verification</h2>
+                <p>Hello ${email}, your verification code is: <strong>${token}</strong></p>
+                <p>If you did not request this, please ignore.</p>
+            </body>
+            </html>`;
         };
+
 
         const emailResponse = await resend.emails.send({
             from: 'Alphagainmetrics@alphagainmetrics.xyz',
             to: email,
-            subject: 'Welcome to Alphagainmetrics',
-            html: welcomeEmailTemplate(email),
+            subject: 'Account Verification',
+            html: authenticateEmailTemplate(email, token),
         });
-
 
         if (!emailResponse) {
             console.log(error)
@@ -384,7 +348,18 @@ module.exports.signup = async (req, res, next) => {
         }
 
         // Save token
-        
+        const savedToken = await new Token({
+            _id: new mongoose.Types.ObjectId(),
+            email,
+            token,
+        }).save();
+
+        if (!savedToken) {
+            let error = new Error("an error occured on the server");
+            error.statusCode = 300;  // Unprocessable Entity (Invalid email)
+            return next(error);
+        }
+
         // Create and save user (no password hashing)
         const newUser = new User({
             _id: new mongoose.Types.ObjectId(),
@@ -397,8 +372,7 @@ module.exports.signup = async (req, res, next) => {
             accountPackage,
             phone,
             isSetPasscode: false,
-            isVerified: false,
-            isEmailVerified:true
+            isVerified: false
         });
 
         let savedUser = await newUser.save();
@@ -425,7 +399,6 @@ module.exports.signup = async (req, res, next) => {
         return next(error);
     }
 };
-
 
 
 module.exports.verifyEmail = async (req, res, next) => {
